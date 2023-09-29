e34f595ccb23832a3fb3007596c495c06fcbfd84
Marco Haber <marcoshaber99@gmail.com>
2023-09-29 10:09:13 +0300

    done with environment setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+    <p className='text-3xl font-medium text-sky-700'>hello world</p>
+
+   <Button variant="destructive">
+    Click me
+   </Button> 
+    </div>
+  )
+}
